ffa47784dde17f378761e4518f7d060a088f2777
Soulxyz <56677594+soulxyz@users.noreply.github.com>
2021-02-19 13:24:44 +0800

    摒除测试代码，增加了显而易见的提示，增加默认值避免繁杂操作

--- manual_main.py
+++ manual_main.py
@@ -17,14 +17,11 @@ class GetHentaiImg():
         gethtml = requests.get(self.http_url)  # Get方式获取网页数据
         self.num_list = self.find_element('<span class="num-pages">(.+?)</span>',gethtml.text)
         self.img_list = ['https://i.nhentai.net/'+ self.find_element('<img src="https://i.nhentai.net/(.+?\.jpg)" width=',gethtml.text)[0]]
-        #self.img_list = self.findall('"objURL":"(\S*?jpg|\S*?JPG)"',html, re.S)
-        
-        print(self.img_list)
+        #print(self.img_list)
         for img_index in range(2,int(self.num_list[0]) + 1):
             self.img_list.append(self.img_list[0].replace('/1.','/' + str(img_index) + '.'))
-            #self.img_list.append(self.img_list.replace('/1.','/' + str(img_index) + '.'))
         for url in self.img_list:
-            #print(url)
+            print(url)
             self.download_queue.put(url, block=False)
 
     def find_element(self,reg,strhtml):
@@ -48,13 +45,13 @@ class GetHentaiImg():
                 img_url = self.download_queue.get()
                 img_url_list = re.findall(re.compile('/([0-9]*).jpg'), img_url)
                 img_save_index = int(img_url_list[0])
-                print("第{}张图片开始下载".format(img_save_index))
+                print("○第{}张图片开始下载".format(img_save_index))
                 try:
                     img_content = requests.get(img_url)
                     filename = '{}/{}.jpg'.format(img_path,img_save_index)
                     with open(filename, 'wb') as f_img:
                         f_img.write(img_content.content)
-                    print("第{}张图片下载完成".format(img_save_index))
+                    print("✔第{}张图片下载完成".format(img_save_index))
                 except Exception as e:
                     print(f'请求出错,地址：{img_url} 错误：{e} ')
                     time.sleep(0.5)
@@ -66,7 +63,7 @@ class GetHentaiImg():
         print("获取图片列表:")
         self.get_base_data()
         print('开始下载')
-        print('--------------------------------------------------------------------------')
+        print('--------------------------------------------------------------------')
         threads = []
 
         for i in range(self.thread_num):
@@ -78,18 +75,17 @@ class GetHentaiImg():
         for t in threads:
             t.join()
 
-        print(f'下载完毕，文件保存在{img_path}')
-        input("爬取完毕,请按回车键退出")
+        print(f'✔下载完毕，文件保存在{img_path}')
+        input("\nPress ENTER to exit.")
 
 if __name__ == '__main__':
     try:
-        #http_url = input("请输入要爬取图册的海报页URL(形如: https://xxxxtai.net/g/297941/): ")
-        http_url = "https://nhentai.net/g/305836/"
-        #thraed_num = input("请输入要使用的线程数(推荐5~10): ")
-        thraed_num = 10
+        http_url = input("请输入要爬取图册的海报页URL(形如: https://xxxxtai.net/g/297941/): ")
+        thraed_num = input("请输入要使用的线程数(默认10): ")
+        if thraed_num == '':thraed_num = 10
         print(http_url,thraed_num)
         crawler = GetHentaiImg(http_url , int(thraed_num) )
         crawler.start()
     except Exception as e:
-        print(f'发生错误，错误信息：{e} ')
+        print(f'✘发生错误，错误信息：{e} ')
         input("Press ENTER to exit.")
